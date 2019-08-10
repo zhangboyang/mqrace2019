d6b981a998968de62ad1f1afc0bc91693d554076
Zhang Boyang <zhangboyang.id@gmail.com>
2019-08-10 14:37:56 +0800

    看一下数据的分布

--- src/main/java/io/openmessaging/DefaultMessageStoreImpl.java
+++ src/main/java/io/openmessaging/DefaultMessageStoreImpl.java
@@ -2,6 +2,8 @@ package io.openmessaging;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 这是一个简单的基于内存的实现，以方便选手理解题意；
@@ -9,21 +11,51 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class DefaultMessageStoreImpl extends MessageStore {
 
-    private NavigableMap<Long, List<Message>> msgMap = new TreeMap<Long, List<Message>>();
+    //private NavigableMap<Long, List<Message>> msgMap = new TreeMap<Long, List<Message>>();
+	
+    // Atomic integer containing the next thread ID to be assigned
+    private static final AtomicInteger nextId = new AtomicInteger(0);
 
+    // Thread local variable containing each thread's ID
+    private static final ThreadLocal<Integer> threadId =
+        new ThreadLocal<Integer>() {
+            @Override protected Integer initialValue() {
+                return nextId.getAndIncrement();
+        }
+    };
+    private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
+    
     @Override
     public synchronized void put(Message message) {
-        if (!msgMap.containsKey(message.getT())) {
+        /*if (!msgMap.containsKey(message.getT())) {
             msgMap.put(message.getT(), new ArrayList<Message>());
         }
 
-        msgMap.get(message.getT()).add(message);
+        msgMap.get(message.getT()).add(message);*/
+    	
+    	if (ThreadLocalRandom.current().nextInt(10000) == 0) {
+	    	StringBuilder s = new StringBuilder();
+	    	s.append(threadId.get());
+	    	s.append(',');
+	    	s.append(Long.toHexString(message.getA()));
+	    	s.append(',');
+	    	s.append(Long.toHexString(message.getT()));
+	    	s.append(',');
+	    	byte[] bytes = message.getBody();
+	    	for (int j = 0; j < bytes.length; j++) {
+	            int v = bytes[j] & 0xFF;
+	            s.append(HEX_ARRAY[v >>> 4]);
+	            s.append(HEX_ARRAY[v & 0x0F]);
+	        }
+	    	
+	    	System.out.println(s);
+    	}
     }
 
 
     @Override
     public synchronized List<Message> getMessage(long aMin, long aMax, long tMin, long tMax) {
-        ArrayList<Message> res = new ArrayList<Message>();
+        /*ArrayList<Message> res = new ArrayList<Message>();
         NavigableMap<Long, List<Message>> subMap = msgMap.subMap(tMin, true, tMax, true);
         for (Map.Entry<Long, List<Message>> mapEntry : subMap.entrySet()) {
             List<Message> msgQueue = mapEntry.getValue();
@@ -34,13 +66,14 @@ public class DefaultMessageStoreImpl extends MessageStore {
             }
         }
 
-        return res;
+        return res;*/
+    	return new ArrayList<Message>();
     }
 
 
     @Override
     public long getAvgValue(long aMin, long aMax, long tMin, long tMax) {
-        long sum = 0;
+        /*long sum = 0;
         long count = 0;
         NavigableMap<Long, List<Message>> subMap = msgMap.subMap(tMin, true, tMax, true);
         for (Map.Entry<Long, List<Message>> mapEntry : subMap.entrySet()) {
@@ -53,7 +86,8 @@ public class DefaultMessageStoreImpl extends MessageStore {
             }
         }
 
-        return count == 0 ? 0 : sum / count;
+        return count == 0 ? 0 : sum / count;*/
+    	return 0;
     }
 
 }
