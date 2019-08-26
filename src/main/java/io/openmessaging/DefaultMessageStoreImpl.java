573542732baa6090f5c79e84240c834bfd63b84c
Zhang Boyang <zhangboyang.id@gmail.com>
2019-08-26 15:12:25 +0800

    20190826 1512

--- src/main/java/io/openmessaging/DefaultMessageStoreImpl.java
+++ src/main/java/io/openmessaging/DefaultMessageStoreImpl.java
@@ -96,8 +96,8 @@ public class DefaultMessageStoreImpl extends MessageStore {
     	});
     }
     
-    private static final String storagePath = "./";
-//    private static final String storagePath = "/alidata1/race2019/data/";
+//    private static final String storagePath = "./";
+    private static final String storagePath = "/alidata1/race2019/data/";
     
     private static final String tAxisPointFile = storagePath + "tAxis.point.data";
     private static final String tAxisBodyFile = storagePath + "tAxis.body.data";
